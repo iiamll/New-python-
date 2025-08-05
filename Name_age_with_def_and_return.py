89edec7fe7468fe0a6ba44546082ea5961791656
Amal <90082080+iiamll@users.noreply.github.com>
2025-08-05 21:59:30 +0530

    Create Name_age_with_def_and_return.py

---
+++ Name_age_with_def_and_return.py
@@ -0,0 +1,9 @@
+Num=input("whats your name ?")
+age=input("whats your age ?")
+def info():
+    A="welcome " + Num
+    B="Good your are " + age + " old"
+    return A,B
+C,D=info()
+print(C)
+print(D)
